8747f4c1d3e417a992e43dedceead5e22a8322a5
MartinBINARD <martin.binard.dev@gmail.com>
2023-08-25 17:40:35 +0200

    Ajout appel API + cityForm en react moderne

--- src/components/CityForm/CityForm.jsx
+++ src/components/CityForm/CityForm.jsx
@@ -1,19 +1,25 @@
-import React from 'react';
+/* eslint-disable react/prop-types */
+import React, { useState } from 'react';
 
-function CityForm({ inputValue, handleChange, handleSubmit }) {
+function CityForm({ setInputValue }) {
+  const [search, setSearch] = useState('');
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setSearch(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setInputValue(search.trim().toLocaleLowerCase());
+  };
+
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        handleSubmit();
-      }}
-    >
+    <form onSubmit={handleSubmit}>
       <input
         placeholder="Search a city ..."
-        value={inputValue}
-        onChange={(e) => {
-          handleChange(e.target.value);
-        }}
+        value={search}
+        onChange={handleChange}
       />
       <button type="submit">OK</button>
     </form>
